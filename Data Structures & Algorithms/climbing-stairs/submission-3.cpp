63e31539cb51ece9549fd6a85696db2153b08a7e
Siva Prasath <sivaprasath6050@gmail.com>
2026-08-30 23:05:20 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,37 @@
+// class Solution {
+// public:
+//     int solve(int n, vector<int> &dp){
+//         if(n == 0 || n == 1) return 1;
+
+//         if(dp[n] != -1) return dp[n];
+
+//         dp[n] = solve(n-1, dp) + solve(n-2, dp);
+//         return dp[n];
+//     }
+
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1, -1);
+
+//         return solve(n , dp);
+//     }
+// };
+
+class Solution {
+public:
+
+    int climbStairs(int n) {
+        if(n == 0 | n == 1) return 1;
+
+        int prev = 1;
+        int prev2 = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curr = prev + prev2;
+
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
+
